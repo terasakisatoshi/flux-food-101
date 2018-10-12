8864b8f509f1b4140629360b3ce648f8a98d719e
Unknown <terasakisatoshi.math@gmail.com>
2018-10-13 04:15:33 +0900

    update

--- src/residue.jl
+++ src/residue.jl
@@ -8,36 +8,30 @@ replace ~/.julia/packages/Flux/UHjNa with all of them under ~/tmp/Flux.jl
 =#
 
 using Flux
-using Flux:Chain, DepthwiseConv
+using Flux:Chain
 using Flux:@treelike
 
-relu6(x) = min(max(zero(x),x), eltype(x)(6))
-
-struct NaiveRes
+struct NaiveBottleNeck
     layers::Chain
     stride::Int
 end
 
-function NaiveRes(ch::Pair{<:Integer,<:Integer}; stride=1)
-    inch = ch[1]
-    hidden = ch[1]
-    outch = ch[2]
-    chain = Chain(Conv((3,3), inch=>hidden, relu6, stride=stride, pad=1),
-                  Conv((3,3), hidden=>hidden, relu6, stride=1, pad=1),
-                  #Conv((3,3), hidden=>hidden, relu6, stride=1, pad=1),
-                  Conv((3,3), inch=>outch, stride=1, pad=1),
+function NaiveBottleNeck(in_ch::Int, middle_ch::Int, out_ch::Int; stride=1)
+    chain = Chain(Conv((1,1), in_ch=>middle_ch, relu),
+                  Conv((3,3), middle_ch=>middle_ch, relu, stride=stride, pad=1),
+                  Conv((1,1), middle_ch=>out_ch),
                   )
-    NaiveRes(chain, stride)
+    NaiveBottleNeck(chain, stride)
 end
 
-@treelike NaiveRes
+@treelike NaiveBottleNeck
 
-function (ec::NaiveRes)(x)
-    h=ec.layers(x)
+function (m::NaiveBottleNeck)(x)
+    h = m.layers(x)
     if size(h)==size(x)
-        h + x
+        relu.(h + x)
     else
-        h
+        relu.(h)
     end
 end
 
@@ -50,16 +44,19 @@ struct NaiveResNet
     layers::Chain
 end
 
-forward() = Chain(Conv((3,3),3=>32, relu6, stride=2,pad=1,),
-                    NaiveRes(32=>64),
-                    NaiveRes(64=>64,stride=2),
-                    NaiveRes(64=>64),
-                    NaiveRes(64=>64,stride=2),
-                    NaiveRes(64=>64),
-                    NaiveRes(64=>64,stride=2),
+forward() = Chain(Conv((7,7), 3=>32, relu, stride=2, pad=3),
+                    NaiveBottleNeck(32, 16, 64),
+                    NaiveBottleNeck(64,32,64,stride=2),
+                    NaiveBottleNeck(64,32,128),
+                    NaiveBottleNeck(128,64,128,stride=2),
+                    NaiveBottleNeck(128,64,256),
+                    NaiveBottleNeck(256,128,256,stride=2),
+                    NaiveBottleNeck(256,128,256),
+                    MaxPool((2,2)),
                     x -> reshape(x, :, size(x, 4)),
-                    Dense(12544,2048),
-                    Dense(2048,101),
+                    Dense(12544, 2048),
+                    Dense(2048, 1024),
+                    Dense(1024, 101),
                     softmax
                     )
 
@@ -73,7 +70,21 @@ NaiveResNet() = NaiveResNet(forward())
 
 using Flux
 using CuArrays
-model = NaiveResNet()
-@show size(model(rand(224, 224, 3, 2)))
-model = model |> gpu
-@show size(model(rand(224, 224, 3, 32)|> gpu))
+
+function test_func()
+    model = NaiveResNet()
+    @show size(model(rand(224, 224, 3, 2)))
+    model = model |> gpu
+    @show size(model(rand(224, 224, 3, 32)|> gpu))
+    batchsize=64
+    loss(x,y)= Flux.crossentropy(model(x), y)
+    total_loss=0
+    for i in 1:500
+        X = rand(224,224,3,batchsize) |> gpu
+        Y = Flux.onehotbatch([rand(1:101) for i in 1:batchsize], 1:101) |> gpu
+        Flux.testmode!(model)
+        total_loss += loss(X, Y).data
+    end
+end
+
+#test_func()
